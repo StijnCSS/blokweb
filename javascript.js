4ba82d7f481496193ec63309614c582d4ad08028
StijnCSS <121390930+StijnCSS@users.noreply.github.com>
2023-10-04 22:12:24 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
-// JavaScript to toggle the menu overlay
-const menuButton = document.querySelector('button'); // Replace with the actual button selector
+// loading vars
+const paginaEen = document.querySelector('.home')
+//menu vars
+const menuButton = document.querySelector('button');
 const menuOverlay = document.getElementById('menu-overlay');
 
 menuButton.addEventListener('click', () => {
@@ -11,31 +13,6 @@ menuButton.addEventListener('click', () => {
         
     }
 });
-
-
-// oude code voor intersection observer
-
-// var button = document.querySelector('button')
-// var body = document.querySelector('orange')
-// var myElement = document.getElementById('my-element');
-// var bounding = myElement.getBoundingClientRect();
-
-// function elementInViewport() {
-
-//     var bounding = myElement.getBoundingClientRect();
-
-//     if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
-        
-//         body.classList.toggle('active');
-//         button
-//         console.log('Element is in the viewport!');
-//     } else {
-
-//         console.log('Element is NOT in the viewport!');
-//     }
-// } 
-
-
 
 // Create an Intersection Observer for the oranje section
 const oranjeObserver = new IntersectionObserver(entries => {
@@ -68,3 +45,41 @@ const zwartObserver = new IntersectionObserver(entries => {
 // Select the zwart footer and start observing it
 const zwartFooter = document.querySelector("footer.zwart");
 zwartObserver.observe(zwartFooter);
+
+
+// de css voegt overflow hidden toe op de body en dit gaat na 2 seconden weg door de class te vewijderen van de body. 
+
+paginaEen.classList.add('no-scroll');
+setTimeout(function() {
+  paginaEen.classList.remove('no-scroll');
+}, 2000); 
+
+
+
+
+
+
+
+
+
+// oude code voor intersection observer
+
+// var button = document.querySelector('button')
+// var body = document.querySelector('orange')
+// var myElement = document.getElementById('my-element');
+// var bounding = myElement.getBoundingClientRect();
+
+// function elementInViewport() {
+
+//     var bounding = myElement.getBoundingClientRect();
+
+//     if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
+        
+//         body.classList.toggle('active');
+//         button
+//         console.log('Element is in the viewport!');
+//     } else {
+
+//         console.log('Element is NOT in the viewport!');
+//     }
+// }
